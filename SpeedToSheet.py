9e8b352e4d11b13d598321da60d6cf899952d2ce
Paul595 <katzensteinerpaul@gmail.com>
2020-06-26 11:56:28 +0200

    Updated Number Format

--- SpeedToSheet.py
+++ SpeedToSheet.py
@@ -34,7 +34,7 @@ def SpeedTest():
     current_time = datetime.now().strftime("%H:%M:%S")
     current_date = datetime.today().strftime('%d/%m/%Y')
 
-    return [current_date, current_time, res["ping"], humanbytes(res["download"]), humanbytes(res["upload"]), res["server"]["id"], res["server"]["name"]+"/"+res["server"]["sponsor"], res["server"]["country"] ]
+    return [current_date, current_time, float(res["ping"]), float(humanbytes(res["download"])), float(humanbytes(res["upload"])), float(res["server"]["id"]), str(res["server"]["name"]+"/"+res["server"]["sponsor"]), str(res["server"]["country"]) ]
 
 
 
